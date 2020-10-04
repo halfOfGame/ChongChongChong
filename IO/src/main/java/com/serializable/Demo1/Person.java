d9e784610a5d67770cbe7ac3de43424dbdfb6640
halfofgame <halfofgame@foxmail.com>
2020-10-04 23:42:01 +0800

    第二次提交。

---
+++ IO/src/main/java/com/serializable/Demo1/Person.java
@@ -0,0 +1,24 @@
+package com.serializable.Demo1;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String name;
+    private int age;
+
+    //我不提供无参构造器
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+
---
+++ Mysql/src/main/java/com.learnsql/basetest/SQLQuery.java
@@ -0,0 +1,32 @@
+package com.learnsql.basetest;
+
+import com.learnsql.DBUtils.DBUtils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLQuery {
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DBUtils.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM table1");
+            while (resultSet.next()) {
+                int TKey = resultSet.getInt(1);
+                String Tname = resultSet.getString(2);
+                int TAge = resultSet.getInt(3);
+                System.out.println(TKey + "\t" + Tname + "\t" + TAge);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.closeALl(resultSet, statement, connection);
+        }
+    }
+}
